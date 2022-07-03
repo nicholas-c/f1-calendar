204c9f83bed905b4e7f4177d1c7b90c7035a8fd3
Nich Carter <nich@Nichs-MacBook-Pro.local>
2022-07-03 23:51:38 +0100

    remove api route

--- src/components/session/session.component.js
+++ src/components/session/session.component.js
@@ -1,31 +1,16 @@
-import clsx from "clsx";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { RaceDataContext } from "../../providers/race-data.context";
 import { RaceCountdown } from "../countdown/countdown.component";
 import { Sessions } from "../race-sessions/race-sessions.component";
-import { Spinner } from "../spinner/spinner.component";
 
 const Session = () => {
   const { nextRace } = useContext(RaceDataContext);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(false);
-  }, [nextRace]);
 
   return (
     <div className="relative  inline-flex  flex-col  p-4  rounded-lg  shadow-md  bg-gray-800  text-white  font-bold  tracking-wider  w-full  md:w-auto">
-      {loading && <Spinner />}
+      <RaceCountdown date={new Date(`${nextRace.date} ${nextRace.time}`)} />
 
-      <div
-        className={clsx("transition-opacity", {
-          "opacity-0": loading,
-        })}
-      >
-        <RaceCountdown date={new Date(`${nextRace.date} ${nextRace.time}`)} />
-
-        <Sessions />
-      </div>
+      <Sessions />
     </div>
   );
 };
