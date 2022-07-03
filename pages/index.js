310b32897e557964bc14279e50849b9e6138902e
Nich Carter <nich@Nichs-MacBook-Pro.local>
2022-07-03 17:08:40 +0100

    change

--- pages/index.js
+++ pages/index.js
@@ -3,120 +3,163 @@ import clsx from "clsx";
 import { add, intervalToDuration } from "date-fns";
 
 export default function Home({ races, nextRace, countdown }) {
-  return (
-    <div className="container  mx-auto  flex  flex-col  md:flex-row  items-center">
-      <div className="w-full  p-5  md:p-10  pt-0  md:pt-10  order-2  md:order-1">
-        <span className="inline-block  py-2  px-4  bg-orange-400  rounded-md  text-sm  text-white  uppercase  font-bold">
-          Next Race
-        </span>
+  const inProgress =
+    new Date() > new Date(`${nextRace.date} ${nextRace.time}`) &&
+    new Date() <
+      add(new Date(`${nextRace.date} ${nextRace.time}`), { hours: 3 });
 
-        <div className="flex  mt-4  mb-2  font-bold">
-          <div className="mr-3  rounded-md  overflow-hidden">
-            <Image
-              src={nextRace.Circuit.flag}
-              className="block"
-              width="40"
-              height="20"
-              alt={nextRace.Circuit.Location.country}
-              priority
-            />
+  if (inProgress) {
+    countdown = {
+      months: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+    };
+  }
+
+  return (
+    <>
+      <div className="container  mx-auto  flex  flex-col  md:flex-row  items-center">
+        <div className="w-full  p-5  md:p-10  pt-0  md:pt-10  order-2  md:order-1">
+          <span
+            className={clsx(
+              "inline-block  py-2  px-4  rounded-md  text-sm  text-white  uppercase  font-bold",
+              {
+                "bg-orange-400": !inProgress,
+                "bg-green-600": inProgress,
+              }
+            )}
+          >
+            {inProgress ? "In progress" : "Next Race"}
+          </span>
+
+          <div className="flex  mt-4  mb-2  font-bold">
+            <div className="mr-3  rounded-md  overflow-hidden">
+              <Image
+                src={nextRace.Circuit.flag}
+                className="block"
+                width="40"
+                height="20"
+                alt={nextRace.Circuit.Location.country}
+                priority
+              />
+            </div>
+
+            {nextRace.Circuit.Location.country}
           </div>
 
-          {nextRace.Circuit.Location.country}
-        </div>
+          <h2 className="text-4xl  font-lato  font-black  uppercase  tracking-wider  mb-6">
+            {nextRace.raceName}
+          </h2>
 
-        <h2 className="text-3xl  font-bold  uppercase  tracking-wider  mb-6">
-          {nextRace.raceName}
-        </h2>
+          <div className="inline-flex  flex-col  p-4  rounded-lg  shadow-md  bg-gray-800  text-white  font-bold  tracking-wider  w-full  md:w-auto">
+            <div className="flex  justify-center  mb-2  border-b  pb-4">
+              {countdown.months > 0 && (
+                <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
+                  <span className="font-black">
+                    {countdown.months < 10
+                      ? "0" + countdown.months
+                      : countdown.months}
+                  </span>{" "}
+                  Months
+                </div>
+              )}
 
-        <div className="inline-flex  flex-col  p-4  rounded-lg  shadow-md  bg-gray-800  text-white  font-bold  tracking-wider  w-full  md:w-auto">
-          <div className="flex  justify-center  mb-2  border-b  pb-4">
-            {countdown.months > 0 && (
               <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
                 <span className="font-black">
-                  {countdown.months < 10
-                    ? "0" + countdown.months
-                    : countdown.months}
+                  {countdown.days < 10 ? "0" + countdown.days : countdown.days}
                 </span>{" "}
-                Months
+                Days
               </div>
-            )}
 
-            <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
-              <span className="font-black">
-                {countdown.days < 10 ? "0" + countdown.days : countdown.days}
-              </span>{" "}
-              Days
+              <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
+                <span className="font-black">
+                  {countdown.hours < 10
+                    ? "0" + countdown.hours
+                    : countdown.hours}
+                </span>{" "}
+                Hours
+              </div>
+
+              <div className="flex  flex-col  items-center  px-4  font-medium">
+                <span className="font-black">
+                  {countdown.minutes < 10
+                    ? "0" + countdown.minutes
+                    : countdown.minutes}
+                </span>{" "}
+                Mins
+              </div>
             </div>
 
-            <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
-              <span className="font-black">
-                {countdown.hours < 10 ? "0" + countdown.hours : countdown.hours}
-              </span>{" "}
-              Hours
-            </div>
+            {nextRace.events.map((event) => {
+              const sessionInProgress =
+                new Date() > new Date(`${event.date} ${event.time}`) &&
+                new Date() <
+                  add(new Date(`${event.date} ${event.time}`), {
+                    hours: 3,
+                  });
 
-            <div className="flex  flex-col  items-center  px-4  font-medium">
-              <span className="font-black">
-                {countdown.minutes < 10
-                  ? "0" + countdown.minutes
-                  : countdown.minutes}
-              </span>{" "}
-              Mins
-            </div>
+              return (
+                <div
+                  key={`current-event-card-` + event.date + event.time}
+                  className={clsx(
+                    "flex  justify-between  mt-1  font-light  text-white",
+                    {
+                      "opacity-25": !(
+                        add(new Date(`${event.date} ${event.time}`), {
+                          hours: 3,
+                        }) > new Date()
+                      ),
+                    }
+                  )}
+                >
+                  <span
+                    className={clsx("w-full  md:pr-32", {
+                      "text-green-600  font-bold": sessionInProgress,
+                    })}
+                  >
+                    {event.name}
+                  </span>
+
+                  <span className="text-gray-400">
+                    {new Date(`${event.date} ${event.time}`).toLocaleString(
+                      "en-GB",
+                      {
+                        weekday: "short",
+                      }
+                    )}
+                  </span>
+
+                  <span className="shrink-0  inline-block  ml-2  bg-gray-500  rounded  w-16  text-center">
+                    {new Date(`${event.date} ${event.time}`).toLocaleString(
+                      "en-GB",
+                      {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      }
+                    )}
+                  </span>
+                </div>
+              );
+            })}
           </div>
+        </div>
 
-          {nextRace.events.map((event) => (
-            <div
-              key={`current-event-card-` + event.date + event.time}
-              className={clsx(
-                "flex  justify-between  mt-1  font-light  text-white",
-                {
-                  "opacity-25": !(
-                    add(new Date(`${event.date} ${event.time}`), { hours: 3 }) >
-                    new Date()
-                  ),
-                }
-              )}
-            >
-              <span
-                className={clsx("w-full  md:pr-32", {
-                  "font-bold":
-                    new Date(`${event.date} ${event.time}`) > new Date(),
-                })}
-              >
-                {event.name}
-              </span>
-
-              <span className="text-gray-400">
-                {new Date(`${event.date} ${event.time}`).toLocaleString(
-                  "en-GB",
-                  {
-                    weekday: "short",
-                  }
-                )}
-              </span>
-
-              <span className="shrink-0  inline-block  ml-2  bg-gray-500  rounded  w-16  text-center">
-                {new Date(`${event.date} ${event.time}`).toLocaleString(
-                  "en-GB",
-                  {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  }
-                )}
-              </span>
-            </div>
-          ))}
+        <div className="w-full  p-5  md:p-10  pb-0  md:pb-10  order-1  md:order-2">
+          <div className="w-3/4  mx-auto">
+            <img src="https://b.kisscc0.com/20180815/vwe/kisscc0-silverstone-circuit-2018-fia-formula-one-world-cha-f1-circuits-2014-2018-silverstone-5b74ebd21307c1.736966211534389202078.png" />
+          </div>
         </div>
       </div>
 
-      <div className="w-full  p-5  md:p-10  pb-0  md:pb-10  order-1  md:order-2">
-        <div className="w-3/4  mx-auto">
-          <img src="https://b.kisscc0.com/20180815/vwe/kisscc0-silverstone-circuit-2018-fia-formula-one-world-cha-f1-circuits-2014-2018-silverstone-5b74ebd21307c1.736966211534389202078.png" />
+      <div className="bg-gray-800">
+        <div className="container  mx-auto  text-white  p-10">
+          <h2 className="text-4xl  font-lato  font-black  uppercase  tracking-wider  mb-6">
+            Previous race
+          </h2>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -129,7 +172,11 @@ export const getServerSideProps = async (ctx) => {
     (race) => new Date(`${race.date} 23:59:59`) > new Date()
   );
 
-  // const nextRace = races[12];
+  const lastRace = await (
+    await fetch("http://ergast.com/api/f1/current/last/results.json")
+  ).json();
+
+  console.log(lastRace.MRData.RaceTable);
 
   nextRace.Circuit.flag = (
     await (
