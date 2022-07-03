6805b69a0af08592d04c2c938deec2067cc735a9
Nich Carter <nich@Nichs-MacBook-Pro.local>
2022-07-03 16:13:26 +0100

    update logic

--- pages/index.js
+++ pages/index.js
@@ -1,279 +1,192 @@
 import Image from "next/image";
 import clsx from "clsx";
-import { intervalToDuration } from "date-fns";
+import { add, intervalToDuration } from "date-fns";
 
-export default function Home({ races }) {
-  const nextRace = races.find(
-    (race) => new Date(`${race.date} ${race.time}`) > new Date()
-  );
-
-  let duration = intervalToDuration({
-    start: new Date(`${nextRace.date} ${nextRace.time}`),
-    end: new Date(),
-  });
-
-  console.log(duration);
-
-  console.log(nextRace);
-
+export default function Home({ races, nextRace, countdown }) {
   return (
-    <div>
-      <div>
-        <div className="container  mx-auto  flex  flex-col  md:flex-row  items-center">
-          <div className="w-full  p-5  md:p-10  pt-0  md:pt-10  order-2  md:order-1">
-            <span className="inline-block  py-2  px-4  bg-orange-400  rounded-md  text-sm  text-white  uppercase  font-bold">
-              Next Race
-            </span>
+    <div className="container  mx-auto  flex  flex-col  md:flex-row  items-center">
+      <div className="w-full  p-5  md:p-10  pt-0  md:pt-10  order-2  md:order-1">
+        <span className="inline-block  py-2  px-4  bg-orange-400  rounded-md  text-sm  text-white  uppercase  font-bold">
+          Next Race
+        </span>
 
-            <div className="flex  mt-4  mb-2  font-bold">
-              <div className="mr-3  rounded-md  overflow-hidden">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/gb.svg"
-                  className="block"
-                  width="40"
-                  height="20"
-                  alt={nextRace.Circuit.Location.country}
-                  priority
-                />
+        <div className="flex  mt-4  mb-2  font-bold">
+          <div className="mr-3  rounded-md  overflow-hidden">
+            <Image
+              src={nextRace.Circuit.flag}
+              className="block"
+              width="40"
+              height="20"
+              alt={nextRace.Circuit.Location.country}
+              priority
+            />
+          </div>
+
+          {nextRace.Circuit.Location.country}
+        </div>
+
+        <h2 className="text-3xl  font-bold  uppercase  tracking-wider  mb-6">
+          {nextRace.raceName}
+        </h2>
+
+        <div className="inline-flex  flex-col  p-4  rounded-lg  shadow-md  bg-gray-800  text-white  font-bold  tracking-wider  w-full  md:w-auto">
+          <div className="flex  justify-center  mb-2  border-b  pb-4">
+            {countdown.months > 0 && (
+              <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
+                <span className="font-black">
+                  {countdown.months < 10
+                    ? "0" + countdown.months
+                    : countdown.months}
+                </span>{" "}
+                Months
               </div>
+            )}
 
-              {nextRace.Circuit.Location.country}
+            <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
+              <span className="font-black">
+                {countdown.days < 10 ? "0" + countdown.days : countdown.days}
+              </span>{" "}
+              Days
             </div>
 
-            <h2 className="text-3xl  font-bold  uppercase  tracking-wider  mb-6">
-              {nextRace.raceName}
-            </h2>
+            <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
+              <span className="font-black">
+                {countdown.hours < 10 ? "0" + countdown.hours : countdown.hours}
+              </span>{" "}
+              Hours
+            </div>
 
-            <div className="inline-flex  flex-col  p-4  rounded-lg  shadow-md  bg-gray-800  text-white  font-bold  tracking-wider  w-full  md:w-auto">
-              <div className="flex  justify-center  mb-2  border-b  pb-4">
-                <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
-                  <span className="font-black">
-                    {duration.days < 10 ? "0" + duration.days : duration.days}
-                  </span>{" "}
-                  Days
-                </div>
-
-                <div className="flex  flex-col  items-center  px-4  border-r  font-medium">
-                  <span className="font-black">
-                    {duration.hours < 10
-                      ? "0" + duration.hours
-                      : duration.hours}
-                  </span>{" "}
-                  Hours
-                </div>
-
-                <div className="flex  flex-col  items-center  px-4  font-medium">
-                  <span className="font-black">
-                    {duration.minutes < 10
-                      ? "0" + duration.minutes
-                      : duration.minutes}
-                  </span>{" "}
-                  Mins
-                </div>
-              </div>
-
-              <div
-                className={clsx(
-                  "flex  justify-between  mt-1  font-light  text-white",
-                  {
-                    "opacity-25": !(
-                      new Date(
-                        `${nextRace.FirstPractice.date} ${nextRace.FirstPractice.time}`
-                      ) > new Date()
-                    ),
-                  }
-                )}
-              >
-                <span>Practice 1</span>
-
-                <span className="text-gray-400">
-                  {new Date(
-                    `${nextRace.FirstPractice.date} ${nextRace.FirstPractice.time}`
-                  ).toLocaleString("en-GB", {
-                    weekday: "short",
-                  })}
-                </span>
-
-                <span className="inline-block  ml-2  bg-gray-500  rounded  px-2">
-                  {new Date(
-                    `${nextRace.FirstPractice.date} ${nextRace.FirstPractice.time}`
-                  ).toLocaleString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </span>
-              </div>
-
-              <div
-                className={clsx(
-                  "flex  justify-between  mt-1  font-light  text-white",
-                  {
-                    "opacity-25": !(
-                      new Date(
-                        `${nextRace.SecondPractice.date} ${nextRace.SecondPractice.time}`
-                      ) > new Date()
-                    ),
-                  }
-                )}
-              >
-                <span>Practice 2</span>
-
-                <span className="text-gray-400">
-                  {new Date(
-                    `${nextRace.SecondPractice.date} ${nextRace.SecondPractice.time}`
-                  ).toLocaleString("en-GB", {
-                    weekday: "short",
-                  })}
-                </span>
-
-                <span className="inline-block  ml-2  bg-gray-500  rounded  px-2">
-                  {new Date(
-                    `${nextRace.SecondPractice.date} ${nextRace.SecondPractice.time}`
-                  ).toLocaleString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </span>
-              </div>
-
-              <div
-                className={clsx(
-                  "flex  justify-between  mt-1  font-light  text-white",
-                  {
-                    "opacity-25": !(
-                      new Date(
-                        `${nextRace.ThirdPractice.date} ${nextRace.ThirdPractice.time}`
-                      ) > new Date()
-                    ),
-                  }
-                )}
-              >
-                <span>Practice 3</span>
-
-                <span className="text-gray-400">
-                  {new Date(
-                    `${nextRace.ThirdPractice.date} ${nextRace.ThirdPractice.time}`
-                  ).toLocaleString("en-GB", {
-                    weekday: "short",
-                  })}
-                </span>
-
-                <span className="inline-block  ml-2  bg-gray-500  rounded  px-2">
-                  {new Date(
-                    `${nextRace.ThirdPractice.date} ${nextRace.ThirdPractice.time}`
-                  ).toLocaleString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </span>
-              </div>
-
-              <div
-                className={clsx(
-                  "flex  justify-between  mt-1  font-light  text-white",
-                  {
-                    "opacity-25": !(
-                      new Date(
-                        `${nextRace.Qualifying.date} ${nextRace.Qualifying.time}`
-                      ) > new Date()
-                    ),
-                  }
-                )}
-              >
-                <span>Qualifying</span>
-
-                <span className="text-gray-400">
-                  {new Date(
-                    `${nextRace.Qualifying.date} ${nextRace.Qualifying.time}`
-                  ).toLocaleString("en-GB", {
-                    weekday: "short",
-                  })}
-                </span>
-
-                <span className="inline-block  ml-2  bg-gray-500  rounded  px-2">
-                  {new Date(
-                    `${nextRace.Qualifying.date} ${nextRace.Qualifying.time}`
-                  ).toLocaleString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </span>
-              </div>
-
-              <div
-                className={clsx(
-                  "flex  justify-between  mt-1  font-light  text-white",
-                  {
-                    "opacity-25": !(
-                      new Date(`${nextRace.date} ${nextRace.time}`) > new Date()
-                    ),
-                  }
-                )}
-              >
-                <span>
-                  Race&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                </span>
-
-                <span className="text-gray-400">
-                  {new Date(`${nextRace.date} ${nextRace.time}`).toLocaleString(
-                    "en-GB",
-                    {
-                      weekday: "short",
-                    }
-                  )}
-                </span>
-
-                <span className="inline-block  ml-2  bg-gray-500  rounded  px-2">
-                  {new Date(`${nextRace.date} ${nextRace.time}`).toLocaleString(
-                    "en-GB",
-                    {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    }
-                  )}
-                </span>
-              </div>
+            <div className="flex  flex-col  items-center  px-4  font-medium">
+              <span className="font-black">
+                {countdown.minutes < 10
+                  ? "0" + countdown.minutes
+                  : countdown.minutes}
+              </span>{" "}
+              Mins
             </div>
           </div>
 
-          <div className="w-full  p-5  md:p-10  pb-0  md:pb-10  order-1  md:order-2">
-            <div className="w-3/4  mx-auto">
-              <img src="https://b.kisscc0.com/20180815/vwe/kisscc0-silverstone-circuit-2018-fia-formula-one-world-cha-f1-circuits-2014-2018-silverstone-5b74ebd21307c1.736966211534389202078.png" />
+          {nextRace.events.map((event) => (
+            <div
+              key={`current-event-card-` + event.date + event.time}
+              className={clsx(
+                "flex  justify-between  mt-1  font-light  text-white",
+                {
+                  "opacity-25": !(
+                    add(new Date(`${event.date} ${event.time}`), { hours: 3 }) >
+                    new Date()
+                  ),
+                }
+              )}
+            >
+              <span
+                className={clsx("w-full  md:pr-32", {
+                  "font-bold":
+                    new Date(`${event.date} ${event.time}`) > new Date(),
+                })}
+              >
+                {event.name}
+              </span>
+
+              <span className="text-gray-400">
+                {new Date(`${event.date} ${event.time}`).toLocaleString(
+                  "en-GB",
+                  {
+                    weekday: "short",
+                  }
+                )}
+              </span>
+
+              <span className="shrink-0  inline-block  ml-2  bg-gray-500  rounded  w-16  text-center">
+                {new Date(`${event.date} ${event.time}`).toLocaleString(
+                  "en-GB",
+                  {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  }
+                )}
+              </span>
             </div>
-          </div>
+          ))}
         </div>
       </div>
 
-      <div>
-        Next Race:{" "}
-        {new Date(`${nextRace.date} ${nextRace.time}`).toLocaleString("en-GB", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}{" "}
-        {nextRace.raceName}
+      <div className="w-full  p-5  md:p-10  pb-0  md:pb-10  order-1  md:order-2">
+        <div className="w-3/4  mx-auto">
+          <img src="https://b.kisscc0.com/20180815/vwe/kisscc0-silverstone-circuit-2018-fia-formula-one-world-cha-f1-circuits-2014-2018-silverstone-5b74ebd21307c1.736966211534389202078.png" />
+        </div>
       </div>
-
-      {races.map((race) => {
-        return (
-          <div key={race.date + race.time}>
-            {race.raceName}
-            {race.date}
-            {race.time}
-          </div>
-        );
-      })}
     </div>
   );
 }
 
-export const getStaticProps = async (ctx) => {
-  const data = await fetch("https://ergast.com/api/f1/2022.json");
+export const getServerSideProps = async (ctx) => {
+  const races = (
+    await (await fetch("https://ergast.com/api/f1/2022.json")).json()
+  ).MRData.RaceTable.Races;
+
+  const nextRace = races.find(
+    (race) => new Date(`${race.date} 23:59:59`) > new Date()
+  );
+
+  // const nextRace = races[12];
+
+  nextRace.Circuit.flag = (
+    await (
+      await fetch(
+        `https://restcountries.com/v3.1/name/${nextRace.Circuit.Location.country.toLowerCase()}`
+      )
+    ).json()
+  ).find(
+    (country) =>
+      country.name.common === nextRace.Circuit.Location.country ||
+      country.altSpellings.includes(nextRace.Circuit.Location.country)
+  ).flags.svg;
+
+  nextRace.events = [
+    {
+      name: "Practice 1",
+      ...nextRace.FirstPractice,
+    },
+    {
+      name: "Practice 2",
+      ...nextRace.SecondPractice,
+    },
+    {
+      name: "Practice 3",
+      ...nextRace.ThirdPractice,
+    },
+    {
+      name: "Qualifying",
+      ...nextRace.Qualifying,
+    },
+    {
+      name: "Sprint",
+      ...nextRace.Sprint,
+    },
+    { name: "Race", date: nextRace.date, time: nextRace.time },
+  ]
+    .filter((event) => event.date)
+    .sort(
+      (a, b) =>
+        new Date(`${a.date} ${a.timne}`) - new Date(`${b.date} ${b.timne}`)
+    );
+
+  let countdown = intervalToDuration({
+    start: new Date(
+      `${nextRace.events.find((event) => event.name === "Race").date} ${
+        nextRace.time
+      }`
+    ),
+    end: new Date(),
+  });
 
   return {
-    revalidate: 300,
+    // revalidate: 300,
     props: {
-      races: (await data.json()).MRData.RaceTable.Races,
+      races,
+      nextRace,
+      countdown,
     },
   };
 };
